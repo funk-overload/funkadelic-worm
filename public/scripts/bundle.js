c2d83e356ddc6f61aa5ac196627f467323aecae9
aggelito <augustherrstrom@gmail.com>
2017-04-23 20:25:15 +0200

    structure startye

--- public/scripts/bundle.js
+++ public/scripts/bundle.js
@@ -78,28 +78,92 @@ module.exports = PIXI;
 
 "use strict";
 
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
 var PIXI = __webpack_require__(0);
-var renderer = PIXI.autoDetectRenderer(800, 600, { backgroundColor: 0x1099bb });
-document.body.appendChild(renderer.view);
-console.log("iuahiwuhdiua");
-var stage = new PIXI.Container();
-var texture = PIXI.Texture.fromImage('sprites/bunny.png');
-var bunny = new PIXI.Sprite(texture);
-bunny.anchor.x = 0.5;
-bunny.anchor.y = 0.5;
-bunny.position.x = 400;
-bunny.position.y = 300;
-bunny.scale.x = 2;
-bunny.scale.y = 2;
-stage.addChild(bunny);
-animate();
-function animate() {
-    requestAnimationFrame(animate);
-    var bunny = stage.getChildAt(0);
-    bunny.rotation += 0.01;
-    renderer.render(stage);
-}
+var GameObject = (function () {
+    function GameObject() {
+    }
+    GameObject.prototype.render = function () {
+    };
+    return GameObject;
+}());
+var GameScene = (function () {
+    function GameScene() {
+        this.init();
+    }
+    GameScene.prototype.init = function () {
+    };
+    GameScene.prototype.update = function () {
+    };
+    GameScene.prototype.renderScene = function () {
+    };
+    return GameScene;
+}());
+var SceneManager = (function () {
+    function SceneManager() {
+    }
+    return SceneManager;
+}());
+var Worm = (function (_super) {
+    __extends(Worm, _super);
+    function Worm() {
+        return _super.call(this) || this;
+    }
+    Worm.prototype.update = function () {
+    };
+    Worm.prototype.render = function () {
+    };
+    return Worm;
+}(GameObject));
+var FunkadelicWorm = (function (_super) {
+    __extends(FunkadelicWorm, _super);
+    function FunkadelicWorm() {
+        return _super.call(this) || this;
+    }
+    FunkadelicWorm.prototype.init = function () {
+    };
+    FunkadelicWorm.prototype.update = function () {
+    };
+    FunkadelicWorm.prototype.renderScene = function () {
+    };
+    return FunkadelicWorm;
+}(GameScene));
+var GameEngine = (function () {
+    function GameEngine() {
+        this.gameDivId = "gameDiv";
+        this.application = new PIXI.Application(800, 600, { backgroundColor: 0x1099bb });
+        document.getElementById(this.gameDivId).appendChild(this.application.view);
+        this.initGameLoop();
+    }
+    GameEngine.prototype.initGameLoop = function () {
+        this.bunny = PIXI.Sprite.fromImage('sprites/bunny.png');
+        this.bunny.anchor.set(0.5);
+        this.bunny.x = this.application.renderer.width / 2;
+        this.bunny.y = this.application.renderer.height / 2;
+        this.bunny.rotation = 1;
+        this.application.stage.addChild(this.bunny);
+        var bunny = this.bunny;
+        this.application.ticker.add(function (delta) {
+            GameEngine.animateBunny(delta, bunny);
+        });
+    };
+    GameEngine.animateBunny = function (delta, bunny) {
+        bunny.rotation += 0.1 * 1;
+    };
+    return GameEngine;
+}());
+GameEngine.sceneManager = new SceneManager();
+var gameEngine = new GameEngine();
 
 
 /***/ })
--- src/typescripts/Game.ts
+++ src/typescripts/Game.ts
@@ -1,23 +1,155 @@
 import * as PIXI from 'pixi.js';
 
-var renderer = PIXI.autoDetectRenderer(800, 600, {backgroundColor: 0x1099bb});
-document.body.appendChild(renderer.view);
-console.log("iuahiwuhdiua");
-var stage = new PIXI.Container();
-var texture = PIXI.Texture.fromImage('sprites/bunny.png');
-var bunny = new PIXI.Sprite(texture);
-bunny.anchor.x = 0.5;
-bunny.anchor.y = 0.5;
-bunny.position.x = 400;
-bunny.position.y = 300;
-bunny.scale.x = 2;
-bunny.scale.y = 2;
-stage.addChild(bunny);
-animate();
 
-function animate() {
-    requestAnimationFrame(animate);
-    var bunny = stage.getChildAt(0);
-    bunny.rotation += 0.01;
-    renderer.render(stage);
+
+
+interface Scene
+{
+	init() : void;
+	update() : void;
+	renderScene() : void;
 }
+interface GraphicObject
+{
+	position : PIXI.Point;
+	anchor : number;
+	rotation: number;
+	width: number;
+	height: number;
+}
+
+class GameObject implements GraphicObject
+{
+	position : PIXI.Point;
+	anchor : number;
+	rotation: number;
+	width: number;
+	height: number;
+
+	render() : void
+	{
+
+	}
+}
+
+abstract class GameScene implements Scene
+{
+	constructor()
+	{
+		this.init();
+	}
+	init() : void
+	{
+
+	}
+	update() : void
+	{
+
+	}
+	renderScene() : void{
+
+	}
+}
+
+class SceneManager
+{
+	scenes : Scene[];
+	constructor()
+	{
+
+	}
+}
+
+class Worm extends GameObject
+{
+
+	constructor()
+	{
+		super();
+	}
+	update()
+	{
+
+	}
+	render()
+	{
+
+	}
+}
+
+//game starts here
+class FunkadelicWorm extends GameScene
+{
+	//main character
+	worm : Worm;
+
+	constructor()
+	{
+		super();
+	}
+	init() : void
+	{
+		
+	}
+	update() : void
+	{
+
+	}
+	renderScene() : void
+	{
+
+	}
+}
+
+class GameEngine
+{
+	gameDivId : string = "gameDiv";
+	application : PIXI.Application;
+
+	renderer : PIXI.WebGLRenderer | PIXI.CanvasRenderer;
+	stage : PIXI.Container;
+
+	bunny : PIXI.Sprite;
+
+	static sceneManager : SceneManager = new SceneManager();
+
+	constructor()
+	{
+		//create view
+		this.application = new PIXI.Application(800, 600, {backgroundColor : 0x1099bb});
+		document.getElementById(this.gameDivId).appendChild(this.application.view);
+
+
+		this.initGameLoop();		
+	}
+
+	initGameLoop() : void
+	{
+		this.bunny = PIXI.Sprite.fromImage('sprites/bunny.png');
+		this.bunny.anchor.set(0.5);
+
+		// move the sprite to the center of the screen
+		this.bunny.x = this.application.renderer.width / 2;
+		this.bunny.y = this.application.renderer.height / 2;
+		this.bunny.rotation = 1;
+
+		this.application.stage.addChild(this.bunny);
+
+		let bunny = this.bunny;
+
+		// Listen for animate update
+		this.application.ticker.add(function(delta : number, ) : void {
+			//if forced to keep animate static, give pointer to objects
+			GameEngine.animateBunny(delta, bunny);
+		});
+	}
+
+
+
+	static animateBunny(delta : number, bunny : PIXI.Sprite) : void
+	{
+		bunny.rotation += 0.1 * 1;
+	}
+}
+
+let gameEngine = new GameEngine();
